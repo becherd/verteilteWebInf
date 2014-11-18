c00b4660dd929bfbbd0e10c6d08ad47ef6fd164f
Hannes Dorfmann <hannes@Hanness-MacBook-Pro-2.local>
2014-11-18 11:32:15 +0100

    Blatt 6

---
+++ Blatt 6/src/main/java/de/tum/Main.java
@@ -0,0 +1,13 @@
+package de.tum;
+
+/**
+ * The main class to start the java app
+ *
+ * @author Hannes Dorfmann
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
