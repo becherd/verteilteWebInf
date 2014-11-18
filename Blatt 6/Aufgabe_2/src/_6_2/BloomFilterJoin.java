a27a345c1946d38dcd073ae3a7afa0bc79243fd4
Hannes Dorfmann <hannes@Hanness-MacBook-Pro-2.local>
2014-11-18 20:40:14 +0100

    BloomFilter

--- Blatt 6/Aufgabe_2/src/_6_2/BloomFilterJoin.java
+++ Blatt 6/Aufgabe_2/src/_6_2/BloomFilterJoin.java
@@ -5,8 +5,10 @@ import com.google.common.hash.Funnel;
 import com.google.common.hash.PrimitiveSink;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 /**
  * @author Hannes Dorfmann
@@ -20,11 +22,11 @@ public class BloomFilterJoin implements DBIterator {
   int joinAttributeIndexRight;
   BloomFilter<Register[]> leftBloomFilter;
   Map<Object, List<Register[]>> leftMap = new HashMap<>();
+  Queue<Register[]> temporaryResult = new LinkedList<>();
 
-  public BloomFilterJoin(DBIterator left, DBIterator right, String joinAttribute) {
+  public BloomFilterJoin(DBIterator left, DBIterator right) {
     this.left = left;
     this.right = right;
-    this.joinAttribute = joinAttribute;
   }
 
   @Override public void close() {
@@ -65,42 +67,91 @@ public class BloomFilterJoin implements DBIterator {
   }
 
   /**
-   *
-   * @param register
+   * Put a register in the left register
    */
-  private void putInLeftMap(Register[] register){
+  private void putInLeftMap(Register[] register) {
     Object key = register[joinAttributeIndexLeft].getObject();
     List<Register[]> found = leftMap.get(key);
-    if (found != null){
+    if (found != null) {
       found = new ArrayList<>();
       leftMap.put(key, found);
     }
 
     found.add(register);
-
   }
 
   private BloomFilter<Register[]> createBloomFilter() {
     return BloomFilter.create(new Funnel<Register[]>() {
       @Override public void funnel(Register[] from, PrimitiveSink into) {
-        into.putInt(from[joinAttributeIndexLeft].getInt());
+        into.putInt(from[0].getInt());
       }
-    }, 10100, 0.001); // S contains ca. 10100 entries
+    }, 10100, 0.01); // S contains ca. 10100 entries
   }
 
   @Override public Register[] next() {
 
-    // Read the right side
+    // If there are joined results not returned yet,
+    // then return them before continue with the next right join
+    if (!temporaryResult.isEmpty()) {
+      return temporaryResult.poll();
+    }
+
+    // Search for next join partner in right table
+    Register[] ret = null;
     Register[] nextRight = null;
 
     while ((nextRight = right.next()) != null) {
 
-      if (leftBloomFilter.mightContain(nextRight)){
-
+      // No join partner, so continue with next
+      if (!leftBloomFilter.mightContain(nextRight)) {
+        continue;
       }
+
+      //
+      List<Register[]> joinables = getJoinPartners(nextRight);
+      for (Register[] left : joinables){
+        Register[] joinResult = createJoinResult(left, nextRight);
+        if (ret == null) {
+          ret = joinResult;
+        } else {
+          temporaryResult.offer(joinResult);
+        }
+      }
+
 
     }
 
-    return new Register[0];
+    // Nothing more to join
+    return null;
+  }
+
+  /**
+   * Get the list of join partners (from left) for the given right side
+   * @param right
+   * @return
+   */
+  private List<Register[]> getJoinPartners(Register[] right){
+    return leftMap.get(right[0]);
+  }
+
+  /**
+   * Creates a join result
+   * @param left
+   * @param right
+   * @return
+   */
+  private Register[] createJoinResult(Register[] left, Register [] right){
+    Register[] result = new Register[left.length + right.length - 1]; // -1 because of joining key
+
+    int pos = 0;
+    for (int i = 0; i< left.length; i++){
+      result[pos++] = left[i];
+    }
+
+    for (int i = 1; i< right.length; i++){
+      result[pos++] = right[i];
+    }
+
+    return result;
   }
 }
